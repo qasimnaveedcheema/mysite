f82c29a93720583272f1250316a3a8ca92c44dbf
qasim <qasimnaveedcheema1985@gmail.com>
2016-11-07 09:02:30 -0800

    deleted comments

--- polls/tests.py
+++ polls/tests.py
@@ -11,7 +11,7 @@ from .models import Question
 def create_question(question_text, days):
     """
     Creates a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
+
     in the past, positive for questions that have yet to be published).
     """
     time = timezone.now() + datetime.timedelta(days=days)
