6b2a6935115126cde98dda77c5ec12e43fc8b4e7
qasim <qasimnaveedcheema1985@gmail.com>
2016-11-07 08:57:33 -0800

    Add test

--- polls/tests.py
+++ polls/tests.py
@@ -1,9 +1,44 @@
 import datetime
-
+from django.urls import reverse
 from django.utils import timezone
 from django.test import TestCase
 
 from .models import Question
+
+
+
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['qlist'], [])
+
+    def test_index_view_with_a_past_question(self):
+        """
+        Questions with a pub_date in the past should be displayed on the
+        index page.
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['qlist'],
+            ['<Question: Past question.>']
+        )
 
 
 class QuestionMethodTests(TestCase):
