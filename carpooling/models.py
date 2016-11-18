921032b96f17b837f0cdb49182b4f2f31b169bee
qasimnaveedcheema <qasimnaveedcheema1985@gmail.com>
2016-11-18 08:53:07 -0800

    modified to test the edit functionaity

--- carpooling/models.py
+++ carpooling/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
 today  = str(datetime.now().date())
-
+#this is a test
 class Registration(models.Model):
 	first_name = models.CharField(max_length=200,default="")
 	last_name = models.CharField(max_length=200,default="")
