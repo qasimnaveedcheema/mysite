d473e00988e0625eecb6fc36d5b61d0ee082d038
qasim <qasimnaveedcheema1985@gmail.com>
2016-11-23 08:12:49 -0800

    Added procfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,6 +149,13 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
